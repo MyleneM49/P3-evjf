e796a22cf007711f3bf14418c7cb498c96901a2f
MyleneM49 <mylene.mercier.simplon@gmail.com>
2022-06-08 16:16:07 +0200

    navbar ok

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,48 +8,17 @@ export default function Header() {
     <div className={style.headerContainer}>
       <div className={style.logoTitleContainer}>
         <div className={style.itemLeft}>
-          <Image
-            src={LogoGlamS2}
-            alt="Logo"
-            width={100}
-            height={100}
-            // layout="fixed"
-          />
+          <Image src={LogoGlamS2} alt="Logo" width={"100%"} height={"100%"} />
         </div>
-        <h1 className={style.itemRight}>EVJF - Burlesque Party</h1>
+        <h1 className={style.itemRight}>EVJF Burlesque</h1>
       </div>
       <div className={style.liens}>
         <Link href="/">Accueil</Link>
         <Link href="/activites">Activités</Link>
         <Link href="/lieux">Lieux</Link>
         <Link href="/formulaire">Réservations</Link>
+        <Link href="/">+</Link>
       </div>
     </div>
-    //HeaderContainer
-    //     <div className="flex flex-col justify-center items-center w-full h-300px bg-red-200 border-pink-500 py-5 px-6 text-[#A53248]">
-    //     // LOGO ET TITRE
-    //         <div className="w-full flex justify-center items-center">
-    //         // LOGO
-    //            <div className="w-1/4 items-center justify-center">
-    //              <Image
-    //              src={LogoRose}
-    //              alt="Logo"
-    //              width={100}
-    //              height={100}
-    //              layout="fixed"
-    //              />
-    //            </div>
-    // //           // TITRE
-    //            <div className="w-3/4 items-center justify-center">
-    //              <h1>EVJF - Burlesque Party</h1>
-    //            </div>
-    //          </div>
-    //        <div className="w-full">
-    //        <Link href="/">Accueil</Link>
-    //        <Link href="/activites">Activités</Link>
-    //        <Link href="/lieux">Lieux</Link>
-    //        <Link href="/formulaire">Réservations</Link>
-    //        </div>
-    //     </div>
   );
 }
--- components/layout.jsx
+++ components/layout.jsx
@@ -1,10 +1,6 @@
 import Head from "next/head";
 import Header from "./Header";
-// import Image from "next/image";
-// import fondAccueil from "../public/fondAccueil.jpg";
 import style from "./style/Layout.module.css";
-
-// import CircleButton from "../components/CircleButton";
 
 export default function Layout({ pageTitle }) {
   return (
@@ -13,41 +9,7 @@ export default function Layout({ pageTitle }) {
         <pagetitle>{pageTitle}</pagetitle>
       </Head>
       <Header />
-      <main className={style.main}>
-        Hasta la vista Baby!! ici image
-        {/* <CircleButton /> */}
-      </main>
+      <main className={style.main}></main>
     </div>
   );
 }
-
-{
-  /* className="flex flex-col w-full h-300px bg-red-200 border-pink-500 py-5 px-6 text-[#A53248]"> */
-}
-{
-  /* <div className="fixed"> */
-}
-{
-  /* // <div className="w-full justify-evenly">
-        // <div className="" */
-}
-//   <Image
-//   src={LogoRose}
-//   alt="Logo"
-//   width={100}
-//   height={100}
-//   layout="fixed"
-// />
-// // </div>
-// // <div className="text-center text-4xl my-4">
-// <h1>EVJF - Burlesque Party</h1>
-// </div>
-// </div>
-// <div className="text-center space-x-5">
-//   <Link href="/">Accueil</Link>
-//   <Link href="/activites">Activités</Link>
-//   <Link href="/lieux">Lieux</Link>
-//   <Link href="/formulaire">Réservations</Link>
-// </div>
-// </div>
-// );
