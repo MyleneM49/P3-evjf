043d01003045e33bea1993f70a706b379aa40ed9
MyleneM49 <mylene.mercier.simplon@gmail.com>
2022-07-07 16:31:20 +0200

    PR

--- pages/formulaire.jsx
+++ pages/formulaire.jsx
@@ -1,5 +1,5 @@
 import Layout from "../components/layout";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styles from "../styles/Home.module.css";
 import HeartButton from "../components/HeartButton";
 
